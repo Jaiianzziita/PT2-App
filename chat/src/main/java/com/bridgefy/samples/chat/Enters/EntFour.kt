0058fda11bb899791799d0c363309d2e8845b80f
Jaia <jaba.plata@hotmail.com>
2021-04-29 18:06:42 -0500

    agregue las dos tablas y lo uní con el chat, todo funciona bien

--- chat/src/main/java/com/bridgefy/samples/chat/Enters/EntFour.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/Enters/EntFour.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import com.bridgefy.samples.chat.MainActivity
 import com.bridgefy.samples.chat.R
+import com.bridgefy.samples.chat.TabsActivity
 
 class EntFour : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +15,7 @@ class EntFour : AppCompatActivity() {
 
 
         findViewById<Button>(R.id.btn4).setOnClickListener({
-            val intent: Intent = Intent (this,  MainActivity::class.java)
+            val intent: Intent = Intent (this,  TabsActivity::class.java)
             startActivity(intent)
         })
     }
