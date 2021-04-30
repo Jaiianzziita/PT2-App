75544175b3d345d9c84bae999ee88ad286bd252f
Jaia <jaba.plata@hotmail.com>
2021-04-30 04:32:31 -0500

    hasta aqui accedemos a la galeria del telefono y preguntamos si se puede acceder o no. Todo esto siguiendo el tutorial -Kotlin clase 11 abriendo galeria de fotos-

--- chat/src/main/java/com/bridgefy/samples/chat/SignupActivity.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/SignupActivity.kt
@@ -40,20 +40,46 @@ class SignupActivity : AppCompatActivity() {
 
     //DETECTAMOS CUANDO SE PULSE EL BOTON PARA ABRIR LA GALERIA
     private fun abreGaleria_Click(){
-        btnGaleria.setOnClickListener(){
-        //muestraGaleria()
+        btnGaleria.setOnClickListener() {
+            //muestraGaleria()
             //VERIFICAMOS QUE VERSION DE ANDROID ESTA INTALADA EN EL TELEFONO
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_DENIED)
-                    //PEDIR PERMISO AL USUARIO
-                     val permisoArchivos = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                requestPermissions(permisoArchivos)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                //PEDIR PERMISO AL USUARIO
+                val permisoArchivos = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permisoArchivos, REQUEST_GALERY)
+            }   else {  //ENTONCES SI TIENE PERMISO
+                    muestraGaleria()
+                }
+        }   else{//TIENE VERSIOND E LOLIPOP HACIA ABAJO Y POR DEFAULT TIENE PERMISO
+                muestraGaleria()
+            }
+            }
+        }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            REQUEST_GALERY->{
+                if(grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                    muestraGaleria()
+                else
+                    Toast.makeText(applicationContext, "No puedes acceder a tus imagenes", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+
     //ABRE LA VENTANA DONDE SE MUESTRA LA GALERIA DE FOTOS
     private fun muestraGaleria(){
-        Toast.makeText(applicationContext, "aqui van las imagenes", Toast.LENGTH_SHORT).show()
+       // Toast.makeText(applicationContext, "Aqui van las imagenes", Toast.LENGTH_SHORT).show()
+        val intentGaleria= Intent(Intent.ACTION_PICK)
+        intentGaleria.type= "image/*"
+        startActivityForResult(intentGaleria, REQUEST_GALERY)
     }
 
 
