50f6fb5272e4ea294062126ec202013f6d17cb73
Jaia <jaba.plata@hotmail.com>
2021-05-22 22:59:01 -0500

    Mapas en Android con Kotlin #3 Controles- y -Mapas en Android con Kotlin #4 Permisos en tiempo de ejecucion

--- chat/src/main/java/com/bridgefy/samples/chat/Maps/Maps.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/Maps/Maps.kt
@@ -1,18 +1,29 @@
 package com.bridgefy.samples.chat.Maps
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.bridgefy.samples.chat.R
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-class Maps : AppCompatActivity(), OnMapReadyCallback {
+class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
 
-    private lateinit var mMap: GoogleMap
+    companion object{
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1//constante para identificar el permiso que estamos pidiendo, puede ser cualquier numero
+    }
+
+    override fun onMarkerClick(p0: Marker?)=false   //con este metodo decimos que queremos hacer con nuestro mapa
+
+
+
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,11 +44,27 @@ class Maps : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        map = googleMap
 
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        map.setOnMarkerClickListener (this)  //this -> que esta actividad se encargara de los mapas
+        map.uiSettings.isZoomControlsEnabled= true      // esto activa los controles de zoom
+
+        setUpMap()
+    }
+
+    private fun setUpMap(){  //este metodo checa que tengamos un permiso en especifico en este caso acces_fine_location
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){ //el packagemanager dice si tenemos un cierto permiso o no
+            ActivityCompat.requestPermissions(this,  // en caso de que no lo tengamos utilizamos el metodo ActivityCompat llamado RequestPermission
+            //se pide un permiso, pero se pueden pedir varios por eso se pone arrayof. En este caso solo se pide el de FINE y se le pasa
+                    // un codigo de nuestro permiso en este caso LOCATION_PERMISSION_REQUIEST_CODE
+                    arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
     }
 }
