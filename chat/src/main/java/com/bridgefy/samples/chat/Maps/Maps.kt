6c30a1980607152c5cd78ced7f6d912a75e8623e
Jaia <jaba.plata@hotmail.com>
2021-05-23 21:17:19 -0500

    Mapas en android con Kotlin #6 Marcadores y tipos de mapas

--- chat/src/main/java/com/bridgefy/samples/chat/Maps/Maps.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/Maps/Maps.kt
@@ -12,8 +12,10 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
 
@@ -64,6 +66,13 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
         setUpMap()
     }
 
+    private fun placeMarker (location: LatLng){
+        val markerOption = MarkerOptions().position(location)
+        markerOption.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE))//cambiar color del marcador
+        map.addMarker(markerOption)
+    }
+
+
     private fun setUpMap(){  //este metodo checa que tengamos un permiso en especifico en este caso acces_fine_location
         if (ActivityCompat.checkSelfPermission(this,
                 android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){ //el packagemanager dice si tenemos un cierto permiso o no
@@ -75,10 +84,12 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
         }
 
         map.isMyLocationEnabled=true //desplegara nuestra ubicacion. Lo que hace es que añade una capa encima de nuestro mapa donde se pone el puntito azul para desplegar la ubicacion del usuario
+       map.mapType = GoogleMap.MAP_TYPE_HYBRID
         fusedLocationClient.lastLocation.addOnSuccessListener (this){location->  //esta llamada se va a ejecutar cuando hayamos obtenido una localizacion del usuario
             if (location!= null){
                 lastLocation=location
                 val currentLatLong = LatLng(location.latitude, location.longitude)
+                placeMarker(currentLatLong)
                 //este metodo se lo vamos a pasar a la siguiente para que se mueva la camara a nuestra ubicacion:
                 map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 13f))//0-20 en zoom donde 0 es el mas alejado, 20 mas cercano.
             }
