c6cb7043ce3bf11a7d7e86bfa8a9470ca359ede3
Jaia <jaba.plata@hotmail.com>
2021-05-23 20:26:34 -0500

    Mapas en Android con Kotlin #5 Localizacion de usuario

--- chat/src/main/java/com/bridgefy/samples/chat/Maps/Maps.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/Maps/Maps.kt
@@ -1,19 +1,24 @@
 package com.bridgefy.samples.chat.Maps
 
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.bridgefy.samples.chat.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
 class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener{
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
 
     companion object{
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1//constante para identificar el permiso que estamos pidiendo, puede ser cualquier numero
@@ -32,6 +37,8 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        fusedLocationClient= LocationServices.getFusedLocationProviderClient(this)
     }
 
     /**
@@ -46,10 +53,10 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
+      /*  // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))           */
 
         map.setOnMarkerClickListener (this)  //this -> que esta actividad se encargara de los mapas
         map.uiSettings.isZoomControlsEnabled= true      // esto activa los controles de zoom
@@ -66,5 +73,16 @@ class Maps : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickLis
                     arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
             return
         }
+
+        map.isMyLocationEnabled=true //desplegara nuestra ubicacion. Lo que hace es que añade una capa encima de nuestro mapa donde se pone el puntito azul para desplegar la ubicacion del usuario
+        fusedLocationClient.lastLocation.addOnSuccessListener (this){location->  //esta llamada se va a ejecutar cuando hayamos obtenido una localizacion del usuario
+            if (location!= null){
+                lastLocation=location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                //este metodo se lo vamos a pasar a la siguiente para que se mueva la camara a nuestra ubicacion:
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 13f))//0-20 en zoom donde 0 es el mas alejado, 20 mas cercano.
+            }
+        }
+
     }
 }
