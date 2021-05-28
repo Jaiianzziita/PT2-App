19dbf2c77d95a2f8e52e236e442fc9ea21d0d118
Jaia <jaba.plata@hotmail.com>
2021-05-27 23:09:38 -0500

    add floatingbutton, en vez de relativeLayout en tab puse CoordinatorLayout, arregle errores que habia, ya hay acceso a broadcast

--- chat/src/main/java/com/bridgefy/samples/chat/Tabs/Tab.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/Tabs/Tab.kt
@@ -1,14 +1,23 @@
 package com.bridgefy.samples.chat.Tabs
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bridgefy.samples.chat.MainActivity
 import com.bridgefy.samples.chat.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_tab.*
 
 class Tab : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tab)
+        val fabb: FloatingActionButton =findViewById(R.id.fabb)
+        fabb.setOnClickListener{view ->
+            val intent: Intent= Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
 
         setUpTabs()
     }
