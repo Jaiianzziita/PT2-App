36520d420fdab91bf6e16b71de5b1d97fd84b6d7
Jaia <jaba.plata@hotmail.com>
2021-06-02 21:47:28 -0500

    119. Como hacer un menu de botones flotantes - floatingActionsMenu

--- chat/src/main/java/com/bridgefy/samples/chat/ForgotPasswordActivity.kt
+++ chat/src/main/java/com/bridgefy/samples/chat/ForgotPasswordActivity.kt
@@ -23,6 +23,7 @@ class ForgotPasswordActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             activity.window.statusBarColor = Color.WHITE
+            
         }
     }
 }
